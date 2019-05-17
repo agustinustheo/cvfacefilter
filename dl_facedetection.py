39be7cf481aa029f1526d2dc935548e74e4d9a01
Agustinus Theodorus <agustinustheoo@gmail.com>
2019-05-18 02:55:19 +0700

    perfected youtube streaming face detection

--- dl_facedetection.py
+++ dl_facedetection.py
@@ -1,5 +1,6 @@
 from imutils.video import VideoStream
 import numpy as np
+import youtube_dl
 import imutils
 import time
 import cv2
@@ -9,66 +10,85 @@ import os
 base_dir = os.path.dirname(__file__)
 prototxt_path = os.path.join(base_dir + 'model/deploy.prototxt')
 caffemodel_path = os.path.join(base_dir + 'model/weights.caffemodel')
+video_url = 'https://www.youtube.com/watch?v=d-YFLAKtcKI'
 
-# load our serialized model from disk
-print("[INFO] loading model...")
-net = cv2.dnn.readNetFromCaffe(prototxt_path, caffemodel_path)
+ydl_opts = {}
 
-# initialize the video stream and allow the cammera sensor to warmup
-print("[INFO] starting video stream...")
-vs = VideoStream('Rick Astley Sings Live - Never Gonna Give You Up - This Morning.mp4').start()
+# create youtube-dl object
+ydl = youtube_dl.YoutubeDL(ydl_opts)
 
-# loop over the frames from the video stream
-while True:
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
-	frame = vs.read()
+# set video url, extract video information
+info_dict = ydl.extract_info(video_url, download=False)
+
+# get video formats available
+formats = info_dict.get('formats',None)
+
+for f in formats:
 	
-	try:
-		frame = imutils.resize(frame, width=400)
-	except:
-		break
+	# I want the lowest resolution, so I set resolution as 480p
+	if f.get('format_note',None) == '480p':
+		
+		#get the video url
+		url = f.get('url',None)
 
-	# grab the frame dimensions and convert it to a blob
-	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
-		(300, 300), (104.0, 177.0, 123.0))
- 
-	# pass the blob through the network and obtain the detections and
-	# predictions
-	net.setInput(blob)
-	detections = net.forward()
+		# load our serialized model from disk
+		print("[INFO] loading model...")
+		net = cv2.dnn.readNetFromCaffe(prototxt_path, caffemodel_path)
 
-	# loop over the detections
-	for i in range(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with the
-		# prediction
-		confidence = detections[0, 0, i, 2]
+		# initialize the video stream and allow the cammera sensor to warmup
+		print("[INFO] starting video stream...")
+		vs = cv2.VideoCapture(url)
 
-		# filter out weak detections by ensuring the `confidence` is
-		# greater than the minimum confidence
-		if confidence < 0.5:
-			continue
+		# loop over the frames from the video stream
+		while True:
+			# grab the frame from the threaded video stream and resize it
+			# to have a maximum width of 400 pixels
+			ret, frame = vs.read()
+			
+			# check if frame is empty
+			if not ret:
+				break
 
-		# compute the (x, y)-coordinates of the bounding box for the
-		# object
-		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-		(startX, startY, endX, endY) = box.astype("int")
- 
-		# draw the bounding box of the face along with the associated
-		# probability
-		text = "{:.2f}%".format(confidence * 100)
-		y = startY - 10 if startY - 10 > 10 else startY + 10
-		cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
-		cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+			# grab the frame dimensions and convert it to a blob
+			(h, w) = frame.shape[:2]
+			blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+				(300, 300), (104.0, 177.0, 123.0))
+		
+			# pass the blob through the network and obtain the detections and
+			# predictions
+			net.setInput(blob)
+			detections = net.forward()
 
-	# show the output frame
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
- 
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+			# loop over the detections
+			for i in range(0, detections.shape[2]):
+				# extract the confidence (i.e., probability) associated with the
+				# prediction
+				confidence = detections[0, 0, i, 2]
+
+				# filter out weak detections by ensuring the `confidence` is
+				# greater than the minimum confidence
+				if confidence < 0.5:
+					continue
+
+				# compute the (x, y)-coordinates of the bounding box for the
+				# object
+				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+				(startX, startY, endX, endY) = box.astype("int")
+		
+				# draw the bounding box of the face along with the associated
+				# probability
+				text = "{:.2f}%".format(confidence * 100)
+				y = startY - 10 if startY - 10 > 10 else startY + 10
+				cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
+				cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+			# show the output frame
+			cv2.imshow("Frame", frame)
+			key = cv2.waitKey(1) & 0xFF
+		
+			# if the `q` key was pressed, break from the loop
+			if key == ord("q"):
+				break
 
 # do a bit of cleanup
 cv2.destroyAllWindows()
--- haar_facedetection.py
+++ haar_facedetection.py
@@ -1,43 +1,72 @@
-import urllib.request
 import os
 import cv2
 import numpy as np
-import pafy
 import youtube_dl
 
-# Define paths
-base_dir = os.path.dirname(__file__)
-face_cascade_path = os.path.join(base_dir + 'model/haarcascade_frontalface_default.xml')
-eye_cascade_path = os.path.join(base_dir + 'model/haarcascade_eye.xml')
+if __name__ == '__main__':
 
-face_cascade = cv2.CascadeClassifier(face_cascade_path)
+    # Define paths
+    base_dir = os.path.dirname(__file__)
+    face_cascade_path = os.path.join(base_dir + 'model/haarcascade_frontalface_default.xml')
+    eye_cascade_path = os.path.join(base_dir + 'model/haarcascade_eye.xml')
+    video_url = 'https://www.youtube.com/watch?v=d-YFLAKtcKI'
 
-eye_cascade = cv2.CascadeClassifier(eye_cascade_path)
+    face_cascade = cv2.CascadeClassifier(face_cascade_path)
 
-cap = cv2.VideoCapture("https://sv3.onlinevideoconverter.com/download?file=a0c2a0f5c2b1b1")
+    eye_cascade = cv2.CascadeClassifier(eye_cascade_path)
 
-if not cap.isOpened():
-    print("File Cannot be Opened")
+    ydl_opts = {}
 
-print(cap)
+    # create youtube-dl object
+    ydl = youtube_dl.YoutubeDL(ydl_opts)
 
+    # set video url, extract video information
+    info_dict = ydl.extract_info(video_url, download=False)
 
-while True:
-    ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray)
-    for (x,y,w,h) in faces:
-        cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+    # get video formats available
+    formats = info_dict.get('formats',None)
 
-    cv2.imshow('img', img)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
+    for f in formats:
+        
+        # I want the lowest resolution, so I set resolution as 144p
+        if f.get('format_note',None) == '360p':
+            
+            #get the video url
+            url = f.get('url',None)
 
-cap.release()
-cv2.destroyAllWindows()
+            # open url with opencv
+            cap = cv2.VideoCapture(url)
+
+            # check if url was opened
+            if not cap.isOpened():
+                print('video not opened')
+                exit(-1)
+
+            while (cap.isOpened()):
+                # read frame
+                ret, frame = cap.read()
+
+                # check if frame is empty
+                if not ret:
+                    break
+
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                faces = face_cascade.detectMultiScale(gray)
+                for (x,y,w,h) in faces:
+                    cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 2)
+                    roi_gray = gray[y:y+h, x:x+w]
+                    roi_color = frame[y:y+h, x:x+w]
+                    eyes = eye_cascade.detectMultiScale(roi_gray)
+                    for (ex, ey, ew, eh) in eyes:
+                        cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+
+                # display frame
+                cv2.imshow('frame', frame)
+                k = cv2.waitKey(30) & 0xff
+                if k == 27:
+                    break
+
+            # release VideoCapture
+            cap.release()
+
+    cv2.destroyAllWindows()
