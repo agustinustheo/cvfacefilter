ee87d530bb0d8673d6fadc78e5d79847dee04bea
Agustinus Theodorus <agustinustheoo@gmail.com>
2019-05-25 16:17:59 +0700

    modified cnn expression model trainer

--- cnn_expression_detection.py
+++ cnn_expression_detection.py
@@ -12,122 +12,125 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #------------------------------
-# #cpu - gpu configuration
-# config = tf.ConfigProto( device_count = {'GPU': 0 , 'CPU': 56} ) #max: 1 gpu, 56 cpu
-# sess = tf.Session(config=config) 
-# keras.backend.set_session(sess)
-# #------------------------------
-# #variables
-# num_classes = 7 #angry, disgust, fear, happy, sad, surprise, neutral
-# batch_size = 256
-# epochs = 5
-# #------------------------------
-# #read kaggle facial expression recognition challenge dataset (fer2013.csv)
-# #https://www.kaggle.com/c/challenges-in-representation-learning-facial-expression-recognition-challenge
+#cpu - gpu configuration
+config = tf.ConfigProto( device_count = {'GPU': 0 , 'CPU': 56} ) #max: 1 gpu, 56 cpu
+sess = tf.Session(config=config) 
+keras.backend.set_session(sess)
+#------------------------------
+#variables
+num_classes = 7 #angry, disgust, fear, happy, sad, surprise, neutral
+batch_size = 256
+epochs = 5
+#------------------------------
+#read kaggle facial expression recognition challenge dataset (fer2013.csv)
+#https://www.kaggle.com/c/challenges-in-representation-learning-facial-expression-recognition-challenge
 
-# with open("resources/fer2013.csv") as f:
-#     content = f.readlines()
+with open("resources/fer2013.csv") as f:
+    content = f.readlines()
 
-# lines = np.array(content)
+lines = np.array(content)
 
-# num_of_instances = lines.size
-# print("number of instances: ",num_of_instances)
-# print("instance length: ", len(lines[1].split(",")[1].split(" ")))
+num_of_instances = lines.size
+print("number of instances: ",num_of_instances)
+print("instance length: ", len(lines[1].split(",")[1].split(" ")))
 
-# #------------------------------
-# #initialize trainset and test set
-# x_train, y_train, x_test, y_test = [], [], [], []
+#------------------------------
+#initialize trainset and test set
+x_train, y_train, x_test, y_test = [], [], [], []
 
-# #------------------------------
-# #transfer train and test set data
-# for i in range(1,num_of_instances):
-#     try:
-#         emotion, img, usage = lines[i].split(",")
+#------------------------------
+#transfer train and test set data
+for i in range(1,num_of_instances):
+    try:
+        emotion, img, usage = lines[i].split(",")
           
-#         val = img.split(" ")
+        val = img.split(" ")
             
-#         pixels = np.array(val, 'float32')
+        pixels = np.array(val, 'float32')
         
-#         emotion = keras.utils.to_categorical(emotion, num_classes)
+        emotion = keras.utils.to_categorical(emotion, num_classes)
     
-#         if 'Training' in usage:
-#             y_train.append(emotion)
-#             x_train.append(pixels)
-#         elif 'PublicTest' in usage:
-#             y_test.append(emotion)
-#             x_test.append(pixels)
-#     except:
-# 	    print("", end="")
+        if 'Training' in usage:
+            y_train.append(emotion)
+            x_train.append(pixels)
+        elif 'PublicTest' in usage:
+            y_test.append(emotion)
+            x_test.append(pixels)
+    except:
+	    print("", end="")
 
-# #------------------------------
-# #data transformation for train and test sets
-# x_train = np.array(x_train, 'float32')
-# y_train = np.array(y_train, 'float32')
-# x_test = np.array(x_test, 'float32')
-# y_test = np.array(y_test, 'float32')
-
-# x_train /= 255 #normalize inputs between [0, 1]
-# x_test /= 255
-
-# x_train = x_train.reshape(x_train.shape[0], 48, 48, 1)
-# x_train = x_train.astype('float32')
-# x_test = x_test.reshape(x_test.shape[0], 48, 48, 1)
-# x_test = x_test.astype('float32')
-
-# print(x_train.shape[0], 'train samples')
-# print(x_test.shape[0], 'test samples')
 #------------------------------
-#construct CNN structure
-# model = Sequential()
+#data transformation for train and test sets
+x_train = np.array(x_train, 'float32')
+y_train = np.array(y_train, 'float32')
+x_test = np.array(x_test, 'float32')
+y_test = np.array(y_test, 'float32')
 
-# #1st convolution layer
-# model.add(Conv2D(64, (5, 5), activation='relu', input_shape=(48,48,1)))
-# model.add(MaxPooling2D(pool_size=(5,5), strides=(2, 2)))
+x_train /= 255 #normalize inputs between [0, 1]
+x_test /= 255
 
-# #2nd convolution layer
-# model.add(Conv2D(64, (3, 3), activation='relu'))
-# model.add(Conv2D(64, (3, 3), activation='relu'))
-# model.add(AveragePooling2D(pool_size=(3,3), strides=(2, 2)))
+x_train = x_train.reshape(x_train.shape[0], 48, 48, 1)
+x_train = x_train.astype('float32')
+x_test = x_test.reshape(x_test.shape[0], 48, 48, 1)
+x_test = x_test.astype('float32')
 
-# #3rd convolution layer
-# model.add(Conv2D(128, (3, 3), activation='relu'))
-# model.add(Conv2D(128, (3, 3), activation='relu'))
-# model.add(AveragePooling2D(pool_size=(3,3), strides=(2, 2)))
-
-# model.add(Flatten())
-
-# #fully connected neural networks
-# model.add(Dense(1024, activation='relu'))
-# model.add(Dropout(0.2))
-# model.add(Dense(1024, activation='relu'))
-# model.add(Dropout(0.2))
-
-# model.add(Dense(num_classes, activation='softmax'))
-# #------------------------------
-# #batch process
-# gen = ImageDataGenerator()
-# train_generator = gen.flow(x_train, y_train, batch_size=batch_size)
-
-# #------------------------------
-
-# model.compile(loss='categorical_crossentropy'
-#     , optimizer=keras.optimizers.Adam()
-#     , metrics=['accuracy']
-# )
-
-# #------------------------------
-
-# fit = True
-
-# if fit == True:
-# 	#model.fit_generator(x_train, y_train, epochs=epochs) #train for all trainset
-# 	model.fit_generator(train_generator, steps_per_epoch=batch_size, epochs=epochs) #train for randomly selected one
-# else:
-# 	model.load_weights('/data/facial_expression_model_weights.h5') #load weights
-
-# model.save('cnn_face_expression.model')
-model = tf.keras.models.load_model('model/cnn_face_expression.model')
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
 #------------------------------
+construct CNN structure
+model = Sequential()
+
+#1st convolution layer
+model.add(Conv2D(64, (5, 5), activation='relu', input_shape=(48,48,1)))
+model.add(MaxPooling2D(pool_size=(5,5), strides=(2, 2)))
+
+#2nd convolution layer
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(AveragePooling2D(pool_size=(3,3), strides=(2, 2)))
+
+#3rd convolution layer
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(AveragePooling2D(pool_size=(3,3), strides=(2, 2)))
+
+model.add(Flatten())
+
+#fully connected neural networks
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.2))
+
+model.add(Dense(num_classes, activation='softmax'))
+#------------------------------
+#batch process
+gen = ImageDataGenerator()
+train_generator = gen.flow(x_train, y_train, batch_size=batch_size)
+
+#------------------------------
+
+model.compile(loss='categorical_crossentropy'
+    , optimizer=keras.optimizers.Adam()
+    , metrics=['accuracy']
+)
+
+#------------------------------
+
+fit = True
+
+if fit == True:
+	#model.fit_generator(x_train, y_train, epochs=epochs) #train for all trainset
+	model.fit_generator(train_generator, steps_per_epoch=batch_size, epochs=epochs) #train for randomly selected one
+else:
+	model.load_weights('/data/facial_expression_model_weights.h5') #load weights
+
+model.save('cnn_face_expression.model')
+#------------------------------
+
+#Load Model
+# model = tf.keras.models.load_model('model/cnn_face_expression.model')
+
 """
 #overall evaluation
 score = model.evaluate(x_test, y_test)
